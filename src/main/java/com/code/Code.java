072d0f0a84c4388a96b8cc7676ea9670a33d82aa
NaveenYadhav <126247158+NaveenYadhav@users.noreply.github.com>
2023-03-17 13:02:52 +0530

    Update Code.java

--- src/main/java/com/code/Code.java
+++ src/main/java/com/code/Code.java
@@ -24,7 +24,46 @@ public class Code {
 		PDFTextStripper str = new PDFTextStripper();
 		String data = str.getText(pddoc);
 		System.out.println(data);
+		
+		 String payeeName = getField(data, "Payee Name");
+		 String nickName = getField(data, "Nickname");
+		 String creditAccountNo=getField(data ,"Credit Account No");
+		 String remarks=getField(data,"Remarks");
+		 String debitAccountNo=getField(data ,"Debit Account");
+		 String date=getField(data,"Date");
+		 String amount=getField(data,"Amount");
+		 String paymentType=getField(data,"Payment Type");
+		 String payVia=getField(data,"Pay via");
+		 	 
+		 
+		
+		AccountDetails td=new AccountDetails(payeeName,nickName,creditAccountNo,remarks,debitAccountNo,date,amount,paymentType,payVia);
+		
+		
+		
+		System.out.println(td.getPayeeName());
+		System.out.println(td.getNickName());
+		System.out.println(td.getCreditAccount());
+		System.out.println(td.getDebitAccount());
+		System.out.println(td.getAmount());
+	    System.out.println(td.getPaymentType());
+		System.out.println(td.getPayVia());
+		System.out.println(td.getTransactionDate());
+		System.out.println(td.getRemarks());
 
 	}
+	
+	 private static String getField(String text, String field) {
+	        int startIndex = text.indexOf(field);
+	       	 if (startIndex == -1) {
+	            return null;
+	        }
+	        int endIndex = text.indexOf("\n", startIndex);
+	         if (endIndex == -1) {
+	            endIndex = text.length();
+	            
+	        }
+	        return text.substring(startIndex + field.length()+1, endIndex).trim();
+	    }
 
 }
